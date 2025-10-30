f100ed841051b5ec2628e9112a2c1c725538b923
majid <MajidSameei1982@GMail.Com>
2025-10-30 13:03:12 +0330

    Add theory and inline attribute

--- tests/Sample.UnitTests/FizzBuzz.UnitTest.cs
+++ tests/Sample.UnitTests/FizzBuzz.UnitTest.cs
@@ -12,28 +12,30 @@ public class FizzBuzzUnitTest
         Assert.Equal(rounds, actual.Count);
     }
 
-    [Fact]
-    public void Start_Should_ReturnAListWithProperFizzValues()
+    [Theory]
+    [InlineData("Fizz", 2)]
+    [InlineData("Buzz", 4)]
+    [InlineData("FizzBuzz", 29)]
+    public void Start_Should_ReturnAListWithProperFizzValues(string expected, int element)
     {
         const int rounds = 100;
         var actual = FizzBuzz.Start(rounds);
-        Assert.Equal("Fizz", actual[2]);
-        Assert.Equal("Fizz", actual[8]);
+        Assert.Equal(expected, actual[element]);
     }
-    [Fact]
-    public void Start_Should_ReturnAListWithProperBuzzValues()
-    {
-        const int rounds = 100;
-        var actual = FizzBuzz.Start(rounds);
-        Assert.Equal("Buzz", actual[4]);
-        Assert.Equal("Buzz", actual[9]);
-    }
-    [Fact]
-    public void Start_Should_ReturnAListWithProperFizzBuzzValues()
-    {
-        const int rounds = 100;
-        var actual = FizzBuzz.Start(rounds);
-        Assert.Equal("FizzBuzz", actual[14]);
-        Assert.Equal("FizzBuzz", actual[29]);
-    }
+    // [Fact]
+    // public void Start_Should_ReturnAListWithProperBuzzValues()
+    // {
+    //     const int rounds = 100;
+    //     var actual = FizzBuzz.Start(rounds);
+    //     Assert.Equal("Buzz", actual[4]);
+    //     Assert.Equal("Buzz", actual[9]);
+    // }
+    // [Fact]
+    // public void Start_Should_ReturnAListWithProperFizzBuzzValues()
+    // {
+    //     const int rounds = 100;
+    //     var actual = FizzBuzz.Start(rounds);
+    //     Assert.Equal("FizzBuzz", actual[14]);
+    //     Assert.Equal("FizzBuzz", actual[29]);
+    // }
 }
